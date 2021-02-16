c20533604c38baa25a3a4ba8f228adf03cf17b1c
MichaelSladedeLange <75497772+MichaelSladedeLange@users.noreply.github.com>
2021-02-16 22:54:30 +0200

    first edition upload

---
+++ java.js
@@ -0,0 +1,13 @@
+
+
+
+$(window).scroll(function() {
+   var hT = $('#scroll-to').offset().top,
+       hH = $('#scroll-to').outerHeight(),
+       wH = $(window).height(),
+       wS = $(this).scrollTop();
+    console.log((hT-wH) , wS);
+   if (wS > (hT+hH-wH)){
+     alert('you have scrolled to the h1!');
+   }
+});
